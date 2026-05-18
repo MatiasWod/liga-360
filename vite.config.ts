aca987e0ecb4c93f8cc1cf7b5e17fa17e1bdfd2c
Matias Wodtke <86923541+MatiasWod@users.noreply.github.com>
2026-05-18 19:56:53 -0300

    chore: remove unused liga360-torneo-form dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-}); 
+	server: {
+		proxy: {
+			'/api/graphql': {
+				target: 'http://localhost:4000',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api\/graphql/, '/graphql'),
+			},
+			'/api/teams': {
+				target: 'http://localhost:4002',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api\/teams/, ''),
+			},
+			'/api/auth': {
+				target: 'http://localhost:4003',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api\/auth/, ''),
+			},
+			'/api/inscriptions': {
+				target: 'http://localhost:4004',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api\/inscriptions/, ''),
+			},
+		},
+	},
+});
